73d19ce10b6ad390976c69301a294d0b88764e41
Viraj Amarasinghe <virajamara@gmail.com>
2024-05-15 20:15:14 +0200

    wip

--- aws/scripts/before-install.sh
+++ aws/scripts/before-install.sh
@@ -3,8 +3,12 @@ set -xe
 
 # Delete the old  directory as needed.
 if [ -d /var/www/html/aws ]; then
-    ls  /var/www/html/aws
+    rm -rf /var/www/html/aws
 fi
 
-# mkdir -vp /var/www/html/aws
+ls /var/www/html
+
+echo "look here......................................."
+
+mkdir -vp /var/www/html/aws
 
